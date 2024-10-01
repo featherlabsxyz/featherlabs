d57f8e218ce27c12cab642170365e1a9c56af1f2
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2024-10-01 14:28:18 +0530

    frontend - deploy

---
+++ apps/web/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+            },
+        ],
+    },
+}
