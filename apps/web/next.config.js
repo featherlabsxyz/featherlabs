f7052221aa6a7c0341fe28dadbfeb2b18e060a91
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2024-10-01 14:28:18 +0530

    frontend - deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,9 +8,4 @@ module.exports = {
             },
         ],
     },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
 }
