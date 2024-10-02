a47aec270b4d7078ec93d0c1172ccdff5cd01790
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2024-10-03 00:25:30 +0530

    create-nft/collection-nft

--- apps/web/src/components/ui/textarea.tsx
+++ apps/web/src/components/ui/textarea.tsx
@@ -1,35 +1,25 @@
-import React, { useState } from "react";
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
 
 export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-    ({ className, value, onChange, ...props }, ref) => {
-        const wordLimit = 1;
-
-        const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-            const words = e.target.value.split(/\s+/);
-            if (words.length <= wordLimit) {
-                onChange && onChange(e);
-            }
-        };
-
+    ({ className, ...props }, ref) => {
         return (
             <textarea
-                className={
+                className={cn(
                     "flex min-h-[60px] border-[0.5px] border-[#888888] rounded-xl bg-[#6D6477] w-full px-3 py-2 text-sm shadow-sm " +
                     "placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed " +
-                    "disabled:opacity-50 " +
+                    "disabled:opacity-50",
                     className
-                }
-                value={value}
-                onChange={handleChange}
+                )}
                 ref={ref}
                 {...props}
             />
-        );
+        )
     }
-);
+)
+Textarea.displayName = "Textarea"
 
-Textarea.displayName = "Textarea";
-
-export { Textarea };
+export { Textarea }
