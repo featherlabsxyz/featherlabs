a82014002b5a0bee2253d796a66037b269aca0fe
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2024-10-03 10:56:20 +0530

    Create NFT | Create Collection NFT | View NFT

--- apps/web/src/app/view-nft/page.tsx
+++ apps/web/src/app/view-nft/page.tsx
@@ -1,9 +1,7 @@
 export default function ViewNFT() {
     return (
         <>
-            <div className="h-screen w-screen flex justify-center items-center">
-
-            </div>
+        4g
         </>
     )
 }
