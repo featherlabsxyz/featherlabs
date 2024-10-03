871292d15fcfdaf0d1d7ac10467d4e601a348e33
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2024-10-03 13:16:53 +0530

    added animations & realtime data change

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
                     ©️copyright Feather Labs 2024. All rights reserved.
                 </div>
                 <div>
-                    <Link href="/" className="flex justify-center items-center">
+                    <Link href="https://x.com/feather_labs" className="flex justify-center items-center">
                         <button role="link"
                                 className="mr-10">
                             <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ export const Footer = () => {
                                 <title>X</title>
                                 <path
                                     d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8 -7.584 -6.638 7.584H0.474l8.6 -9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-                                    fill="#ffffff" stroke-width="1"></path>
+                                    fill="#bebebe" stroke-width="1"></path>
                             </svg>
                         </button>
                     </Link>
--- apps/web/components/NFTCard.tsx
+++ apps/web/components/NFTCard.tsx
@@ -1,14 +1,54 @@
-export default function NFTCard({ image }: any) {
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+interface NFTCardProps {
+    image: string | null;
+    name: string;
+    description: string;
+    website: string;
+}
+
+export default function NFTCard({ image, name, description, website }: NFTCardProps) {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     return (
-        <div className="relative mx-4 drop-shadow-2xl bg-[#5A5465] font-new-black rounded-2xl text-5xl overflow-hidden">
-            <div className="h-[590px] relative z-30">
-                <div className="relative z-10 p-10">
-                    <h2 className="text-center text-[30px]">NFT Card Title</h2>
-                    {image && (
-                        <div className="mt-4 flex justify-center">
-                            <img src={image} alt="Uploaded NFT" className="max-w-full max-h-[400px] object-contain rounded-lg" />
-                        </div>
-                    )}
+        <div className="w-full sticky top-10 pt-8 mx-auto">
+            <div
+                className={`relative mx-4 font-new-black rounded-2xl text-5xl overflow-hidden h-[590px] mb-4 bg-[#5A5465] ${
+                    imageLoaded ? "drop-shadow-2xl" : ""
+                }`}
+            >
+                {image && (
+                    <motion.div
+                        initial={{ opacity: 0, filter: "blur(50px)" }}
+                        animate={{ opacity: 1, filter: "blur(0)" }}
+                        exit={{
+                            opacity: 0,
+                            filter: "blur(10px)",
+                            transition: { duration: 0.5 },
+                        }}
+                        className="absolute inset-0 w-full h-full"
+                    >
+                        <Image
+                            src={image}
+                            alt="Uploaded NFT"
+                            width={590}
+                            height={590}
+                            className="w-full h-full object-cover z-0"
+                            onLoadingComplete={() => setImageLoaded(true)}
+                        />
+                    </motion.div>
+                )}
+
+                {imageLoaded && (
+                    <div className="absolute inset-x-0 bottom-0 h-1/4 bg-gradient-to-t from-black/60 to-transparent pointer-events-none"></div>
+                )}
+
+                <div className="absolute bottom-0 z-10 p-4 text-white w-full">
+                    <div className="font-new-black text-[30px] font-semibold">{name}</div>
+                    <div className="text-sm truncate">{description}</div>
+                    <div className="text-sm underline mt-4">{website}</div>
                 </div>
             </div>
         </div>
