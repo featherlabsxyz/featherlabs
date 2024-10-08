fdc2cc46f65ae746b383e77f7c4ccffc455a5853
Fahad Ahmad <meimfhd@Fahad-ka-MacBook-Air.local>
2024-10-09 01:49:51 +0530

    fix

--- apps/web/src/app/view-nft/page.tsx
+++ apps/web/src/app/view-nft/page.tsx
@@ -1,8 +1,4 @@
-import { useWallet } from "@solana/wallet-adapter-react";
-
 export default function ViewNFT() {
-  const { publicKey } = useWallet();
-
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center"></div>
